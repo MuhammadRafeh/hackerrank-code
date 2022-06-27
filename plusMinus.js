dcaa48af3aec5577711f3ba38960c6cc14ae8e46
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2022-06-27 17:57:31 +0500

    Updated and Successfully done with PlusMinus Challenge!

--- plusMinus.js
+++ plusMinus.js
@@ -1,12 +1,17 @@
-function adjustingFloat(ratio) {
-    const remainingLength = Math.abs(8 - ratio.toString().length);
-    if (remainingLength == 0) {
+function adjustingFloat(ratio){
+    let remainingLength = Math.abs(8 - ratio.toString().length);
+    if (remainingLength == 0){
         return ratio.toString();
     }
     let dupRatio = ratio.toString();
-    for (let i = 0; i < remainingLength; i++) {
+    if (!dupRatio.includes('.')){
+        dupRatio += '.';
+        remainingLength -= 1;
+    }
+    for (let i = 0; i< remainingLength; i++){
         dupRatio += '0';
     }
+    
     return dupRatio
 }
 
@@ -16,24 +21,24 @@ function plusMinus(arr) {
     let negativeStack = 0;
     let positiveStack = 0;
     let numberOfZeros = 0;
-
+    
     arr.forEach(number => {
-        if (number < 0) {
+        if (number < 0){
             negativeStack += 1;
-        } else if (number > 0) {
-            positiveStack += 1;
+        } else if (number > 0){
+            positiveStack += 1;         
         } else { // number = 0
             numberOfZeros += 1;
         }
     })
     let positiveRatio = +(positiveStack / totalLength).toFixed(6);
     let negativeRatio = +(negativeStack / totalLength).toFixed(6);
-    let zeroRatio = +(numberOfZeros / totalLength).toFixed(6);
+    let zeroRatio =+(numberOfZeros / totalLength).toFixed(6);
     console.log(adjustingFloat(positiveRatio))
     console.log(adjustingFloat(negativeRatio))
     console.log(adjustingFloat(zeroRatio))
 }
 
-const arr = [1, 1, 0, -1, -1];
+const arr = [1, 1, 4,63,26,3];
 
 plusMinus(arr)
